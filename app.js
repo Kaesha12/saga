d2288d6d4488d3235f5d5c840559d5f097c142bc
Kaesha12 <119775049+Kaesha12@users.noreply.github.com>
2023-09-25 13:23:17 +0700

    test ke 3

--- app.js
+++ app.js
@@ -8,38 +8,42 @@ let N = 0
 let A = 0
 let Ro = 0
 
-function btn () {
-    var audio = document.createElement('audio')
-        const bark = Math.random()*33
-        count +=1
-        R.src='media/saga-2.png'
-        L.src='media/saga-3.png'
-        setTimeout(function(){
-         R.src ='media/saga-1.png'
-         L.src ='media/saga-4.png'
-        },200)
-    if (count>5){
-        R.classList.toggle('hidden')
-        L.classList.toggle('hidden')
-        count-=5
-    }
-    if (bark < 11){
-        A +=1
-        aburage.innerText = A
-        audio.setAttribute('src','media/aburage.mp3')
-        audio.play()
-    }
-    else if (bark < 21){
-        N +=1
-        natto.innerText = N
-        audio.setAttribute('src','media/natogohan.mp3')
-        audio.play()
-    }
-    else if (20 < bark){
-        Ro +=1
-        rokong.innerText = Ro
-        audio.setAttribute('src','media/rokongsojo.mp3')
-        audio.play()
-    }
+
+window.addEventListener("load", () => {
+  const load = document.querySelector(".load");
+  load.classList.add("hidden");
+});
+
+function btn() {
+  var audio = document.createElement("audio");
+  const bark = Math.random() * 33;
+  count += 1;
+  R.src = "media/saga-2.png";
+  L.src = "media/saga-3.png";
+  setTimeout(function () {
+    R.src = "media/saga-1.png";
+    L.src = "media/saga-4.png";
+  }, 1000);
+  if (count > 5) {
+    R.classList.toggle("hidden");
+    L.classList.toggle("hidden");
+    count -= 5;
+  }
+  if (bark < 11) {
+    A += 1;
+    aburage.innerText = A;
+    audio.setAttribute("src", "media/aburage.mp3");
+    audio.play();
+  } else if (bark < 21) {
+    N += 1;
+    natto.innerText = N;
+    audio.setAttribute("src", "media/natogohan.mp3");
+    audio.play();
+  } else if (20 < bark) {
+    Ro += 1;
+    rokong.innerText = Ro;
+    audio.setAttribute("src", "media/rokongsojo.mp3");
+    audio.play();
+  }
 }
 
