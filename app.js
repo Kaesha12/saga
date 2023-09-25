2ac7459719102b5c82d0b84f33612cd9080f512e
Kaesha12 <119775049+Kaesha12@users.noreply.github.com>
2023-09-25 14:33:09 +0700

    mobile

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
-const R = document.getElementById('R')
-const L = document.getElementById('L')
-const natto = document.getElementById('natto')
-const aburage = document.getElementById('aburage')
-const rokong = document.getElementById('rokong')
-let count = 0
-let N = 0
-let A = 0
-let Ro = 0
+const title = document.getElementById("title");
+const R = document.getElementById("R");
+const L = document.getElementById("L");
+const natto = document.getElementById("natto");
+const aburage = document.getElementById("aburage");
+const rokong = document.getElementById("rokong");
+let count = 0;
+let N = 0;
+let A = 0;
+let Ro = 0;
 
-  window.addEventListener("load", () => {
-    const load = document.querySelector(".load");
-    setTimeout(function () {
-      load.classList.add("hidden");
-    }, 3000);
-  });
-  function btn() {
-    var audio = document.createElement("audio");
-    const bark = Math.random() * 33;
-    count += 1;
-    R.src = "media/saga-2.png";
-    L.src = "media/saga-3.png";
-    setTimeout(function () {
-      R.src = "media/saga-1.png";
-      L.src = "media/saga-4.png";
-    }, 150);
-    if (count > 5) {
-      R.classList.toggle("hidden");
-      L.classList.toggle("hidden");
-      count -= 5;
-    }
-    if (bark < 11) {
-      A += 1;
-      aburage.innerText = A;
-      audio.setAttribute("src", "media/aburage.mp3");
-      audio.play();
-    } else if (bark < 21) {
-      N += 1;
-      natto.innerText = N;
-      audio.setAttribute("src", "media/natogohan.mp3");
-      audio.play();
-    } else if (20 < bark) {
-      Ro += 1;
-      rokong.innerText = Ro;
-      audio.setAttribute("src", "media/rokongsojo.mp3");
-      audio.play();
-    }
+window.addEventListener("load", () => {
+  const load = document.querySelector(".load");
+  setTimeout(function () {
+    load.classList.add("hidden");
+  }, 2000);
+});
+function btn() {
+  var audio = document.createElement("audio");
+  const bark = Math.random() * 33;
+  count += 1;
+  R.src = "media/saga-2.png";
+  L.src = "media/saga-3.png";
+  setTimeout(function () {
+    R.src = "media/saga-1.png";
+    L.src = "media/saga-4.png";
+  }, 300);
+  if (count > 5) {
+    R.classList.toggle("hidden");
+    L.classList.toggle("hidden");
+    count -= 5;
   }
-
+  if (bark < 11) {
+    A += 1;
+    aburage.innerText = A;
+    audio.setAttribute("src", "media/aburage.mp3");
+    audio.play();
+  } else if (bark < 21) {
+    N += 1;
+    natto.innerText = N;
+    audio.setAttribute("src", "media/natogohan.mp3");
+    audio.play();
+  } else if (20 < bark) {
+    Ro += 1;
+    rokong.innerText = Ro;
+    audio.setAttribute("src", "media/rokongsojo.mp3");
+    audio.play();
+  }
+  if (N + A + Ro == 3) {
+    title.innerText = "super duper Doggo";
+  }
+}
